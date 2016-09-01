4d255c344f237687fdd6696b225c2a0e86dcdcf5
Jason Warta <jasonwarta@gmail.com>
2016-08-31 18:56:23 -0800

    removed unnecessary code and fixed and error with directory creation"

--- makemkv.sh
+++ makemkv.sh
@@ -8,8 +8,30 @@ else
 fi
 
 if [ -n "$volName" ]; then
-	mkdir /Volumes/JasonsMedia/vidout/$volName
-	makemkvcon mkv disc:0 all --minlength 3600 /Volumes/JasonsMedia/vidout/$volName 1>/Volumes/JasonsMedia/vidout/$volName/log.txt 2>/Volumes/JasonsMedia/vidout/$volName/error.txt
+	mkdir /Volumes/JasonsMedia/vidout/"$volName" >/dev/null
+	makemkvcon mkv disc:0 all --minlength 3600 --messages=-stdout --progress=-stdout --cache=512 /Volumes/JasonsMedia/vidout/"$volName"
+
+	# toggle=true
+	# sleep 1;
+
+	# while pgrep makemkvcon > /dev/null;
+	# do
+	# 	# check if tail is running, start if not
+	# 	if $toggle; then
+	# 		tail -f -n 1 /Volumes/JasonsMedia/vidout/$volName/log.txt & 
+	# 		toggle=false
+	# 	fi
+		
+	# 	sleep 5;
+
+	# 	# if handbrake has finished running, stop tail
+	# 	if ! pgrep makemkvcon > /dev/null; then
+	# 		kill $!
+	# 		wait $! 2>/dev/null
+	# 	fi
+		
+	# done;
+
 	diskutil eject $id
 else
 	echo "Couldn't process disk"
