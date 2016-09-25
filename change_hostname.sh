46a3724892bc762c7bace78ed07c38c3b85771bc
Jason Warta <jasonwarta@gmail.com>
2016-09-24 21:31:03 -0800

    fixed hostname script

--- change_hostname.sh
+++ change_hostname.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 new_name=$1
-old_name=$(hostname)
+old_name=`hostname`
 
 sudo hostname $new_name
-sudo sed -i 's/$old_name/$new_name/g' /etc/hostname
-sudo sed -i 's/$old_name/$new_name/g' /etc/hosts
+sed -i 's/$old_name/$new_name/g' /etc/hostname
+sed -i 's/$old_name/$new_name/g' /etc/hosts
