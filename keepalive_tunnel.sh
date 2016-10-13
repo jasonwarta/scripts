4c16f063f04be6a6088ac4a0f2bd9312d3bd5473
Jason Warta <jasonwarta@gmail.com>
2016-10-12 18:12:26 -0800

    added modified version of keepalive for ssh tunnel proxy

---
+++ keepalive_tunnel.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# keepalive script for restarting python server when it breaks
+# Usage: place in same dir as server.py and run from there
+
+tunnel='ssh -D 8080 -f -C -q -N HS -p 22'
+
+PID=""
+if ! pgrep -f 'ssh -D 8080 -f -C -q -N HS -p 22' 2>&1 > /dev/null; then
+	echo "tunnel wasn't running."
+	echo "starting server"
+	$tunnel
+	PID=$!
+else
+	echo "server was already running"
+	echo "attaching to server process"
+	PID=$(pgrep -f 'ssh -D 8080 -f -C -q -N HS -p 22')
+fi
+
+
+function terminate {
+	kill $PID
+	exit
+}
+trap terminate SIGHUP SIGINT SIGKILL SIGTERM SIGSTOP
+
+
+while true; do
+	while pgrep -f 'ssh -D 8080 -f -C -q -N HS -p 22' 2>&1 > /dev/null; do
+		sleep 1
+	done
+
+	echo -e "$(date): Restarting socks tunnel\n" >> ~/logs/socks_tunnel.log
+	$tunnel
+	PID=$!
+done
