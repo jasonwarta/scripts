9f6b62a1c92d25cf3e4cb8a5f3e1b87565c265fd
Jason Warta <jasonwarta@gmail.com>
2016-11-14 14:14:09 -0900

    added better arg handling

--- bash_essentials.sh
+++ bash_essentials.sh
@@ -1,5 +1,8 @@
+# useful batch functions and scripts for use in a vareity of programs
+
+# confirm function, takes y/n
 confirm () {
-    read -r -p "${1:-Are you sure? [y/N]} " response
+    read -r -p "${1:-Are you sure? [y/N]} " response </dev/tty
     case $response in
         [yY][eE][sS]|[yY]) 
             true
@@ -10,8 +13,28 @@ confirm () {
     esac
 }
 
+
+# catches Ctrl+C and other kill commands
 function cleanup {
 	exit
 }
-
 trap cleanup SIGHUP SIGINT SIGKILL SIGTERM SIGSTOP
+
+
+# option input for generic program
+for i in "$@"; do
+    case $i in
+        -t=*|--title=*)
+        title="${i#*=}"
+        shift # past argument=value
+        ;;
+        -y=*|--year=*)
+        year="${i#*=}"
+        shift # past argument=value
+        ;;
+        -e=*|--extension=*)
+        extension="${i#*=}"
+        shift # past argument=value
+        ;;
+    esac
+done
