48ae1e30ae4a22ac4d2325dd996dcfcc3714c91e
Jason Warta <jasonwarta@gmail.com>
2016-11-14 20:38:03 -0900

    added help option

--- movie_rename.sh
+++ movie_rename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 confirm () {
-    read -r -p "${1:-Are you sure? [y/N]} " response
+    read -r -p "${1:-Are you sure you want to rename the file? [y/N]} " response </dev/tty
     case $response in
         [yY][eE][sS]|[yY]) 
             true
@@ -32,28 +32,48 @@ for i in "$@"; do
 	    extension="${i#*=}"
 	    shift # past argument=value
 	    ;;
+	    -h|--help)
+		echo "Movie Rename"
+		echo "usage: movie_rename [options]"
+		echo
+		echo "Options:"
+		echo "  -t=<title>, --title=<title> 			provide a title"
+		echo "  -y=<year>, --year=<year> 			provide a year"
+		echo "  -e=<extension>, --extension=<extension>    	provide an extension"
+		echo "  -h, --help         				display this help"
+		echo
+		echo "You will be promted before renaming a file."
+		exit
+		shift
+		;;
 	esac
 done
 
-for file in "`ls {*.mp4,*.mkv,*.avi} 2>/dev/null`"; do
+# for file in "`ls {*.mp4,*.mkv,*.avi} 2>/dev/null`"; do
+ls {*.mp4,*.mkv,*.avi} 2>/dev/null |
+while read file; do
 
 	if [ -z $title ]; then
-		title="`echo "$file"|sed 's/\./ /g'|sed 's/\([A-Za-z'\'']*\([ ]*[A-Za-z'\'']\)*\).*/\1/'`";
+		title="`sed 's/\./ /g; s/\([A-Za-z'\'']*\([ ]*[A-Za-z'\'']\)*\).*/\1/' <<< "$file"`";
 	fi
 	if [ -z $year ]; then
-		year="`echo "$file"| sed 's/.*\([12][0-9]\{3\}\).*/\1/'`";
+		year="`sed 's/.*\([12][0-9]\{3\}\).*/\1/' <<< "$file"`";
 	fi
 	if [ -z $extension ]; then
-		extension="`echo "$file"|sed 's/.*\(\....\)/\1/'`";
+		extension="`sed 's/.*\(\....\)/\1/' <<< "$file"`";
 	fi
 	# title="`echo "$file"|sed 's/\./ /g'|sed 's/\([A-Za-z]*\([ ]*[A-Za-z]\)*\).*/\1/'`";
 	# year="`echo "$file"| sed 's/^.*\([0-9]\{4\}\).*/\1/'`";
 	# extension="`echo "$file"| sed 's/.*\(\.[A-Za-z0-9]\{3\}$\)/\1/'`";
-	echo "title :$title"
-	echo "year :$year"
-	echo "extension :$extension"
+	echo "title: $title"
+	echo "year: $year"
+	echo "extension: $extension"
 
 	fname="$title ($year)$extension"
 	echo "new filename: \"$fname\""
 	confirm && mv "$file" "$fname"
+
+	unset title
+	unset year
+	unset extension
 done
