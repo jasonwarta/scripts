a9e6354ec6568c28751d21b19365c1f3c9001041
Jason Warta <jasonwarta@gmail.com>
2016-12-29 22:48:28 -0900

    refined, added features and special character handling

--- retag.sh
+++ retag.sh
@@ -9,13 +9,22 @@ else
 	outputDir=${args[0]}
 
 	for (( i=1;i<$numOfElems;i++ )); do
-		name="$(mediainfo "${args[${i}]}"|grep -e 'Track name *: '|sed 's/Track name *: //')"
+
+		size="$(stat --format="%s" "${args[${i}]}")"
+		if [ "$size" = "0" ];then
+			# echo "size 0: ${args[${i}]}"
+			rm "${args[${i}]}"
+			continue
+		fi
+
+		name="$(mediainfo "${args[${i}]}"|grep -e 'Track name *: '|sed 's/Track name *: //')" 2>/dev/null
+		name="$(sed 's,/,-,'<<<$name)"
 		# echo "$name"
-		artist="$(mediainfo "${args[${i}]}"|grep -e '^Performer *: '|sed 's/^Performer *: //')"
+		artist="$(mediainfo "${args[${i}]}"|grep -e '^Performer *: '|sed 's/^Performer *: //')" 2>/dev/null
 		# echo "$artist"
-		album="$(mediainfo "${args[${i}]}"|grep -e 'Album *: '|sed 's/Album *: //')"
+		album="$(mediainfo "${args[${i}]}"|grep -e 'Album *: '|sed 's/Album *: //')" 2>/dev/null
 		# echo "$album"
-		track="$(mediainfo "${args[${i}]}"|grep -e 'Track name/Position *: '|sed "s,Track name/Position *: ,,")"
+		track="$(mediainfo "${args[${i}]}"|grep -e 'Track name/Position *: '|sed "s,Track name/Position *: ,,")" 2>/dev/null
 		printf -v track "%02d" $track
 		# echo "$track"
 		ext="$(sed 's/.*\(\....$\)/\1/'<<<${args[${i}]})"
@@ -26,10 +35,14 @@ else
 
 		# echo "Path: $fpath"
 		# echo "Name: $fname"
-
-		mkdir -p "$outputDir/$fpath"
-		mv "${args[${i}]}" "$fname" 2>/dev/null
-		mv "$fname" "$outputDir/$fpath/" 
-		echo "${args[${i}]}  >>>>  $outputDir/$fpath/$fname"
+		
+		if [ -z "$name" ];then
+			echo "couldn't get info for $file"
+		else
+			mkdir -p "$outputDir/$fpath"
+			mv "${args[${i}]}" "$fname" 2>/dev/null
+			mv "$fname" "$outputDir/$fpath/" 
+			echo "${args[${i}]}  >>>>  $outputDir/$fpath/$fname"
+		fi
 	done
 fi
