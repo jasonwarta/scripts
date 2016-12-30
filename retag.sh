259fef268aa66116eb428a3944c9c7d98352180d
Jason Warta <jasonwarta@gmail.com>
2016-12-29 15:28:34 -0900

    added tagging script. reads file tags and renames files

--- retag.sh
+++ retag.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#mediainfo AAAL.mp3|grep -e 'Track name *:'|sed 's/Track name *: //'
-
-#get filename
-f="$1"
-
-
-echo $f
-
 args=("$@")
 if [ $# -eq 0 ] || [ $# -eq 1 ]; then
 	echo "Correct usage:"
@@ -16,11 +8,28 @@ else
 	numOfElems=${#args[@]}
 	outputDir=${args[0]}
 
-	for (( i=1;i$numOfElems;i++ )); do
+	for (( i=1;i<$numOfElems;i++ )); do
 		name="$(mediainfo "${args[${i}]}"|grep -e 'Track name *: '|sed 's/Track name *: //')"
-		artist="$(mediainfo "${args[${i}]}"|grep -e 'Performer *: '|sed 's/Performer *: //')"
+		# echo "$name"
+		artist="$(mediainfo "${args[${i}]}"|grep -e '^Performer *: '|sed 's/^Performer *: //')"
+		# echo "$artist"
 		album="$(mediainfo "${args[${i}]}"|grep -e 'Album *: '|sed 's/Album *: //')"
+		# echo "$album"
+		track="$(mediainfo "${args[${i}]}"|grep -e 'Track name/Position *: '|sed "s,Track name/Position *: ,,")"
+		printf -v track "%02d" $track
+		# echo "$track"
+		ext="$(sed 's/.*\(\....$\)/\1/'<<<${args[${i}]})"
+		# echo "$ext"
 
-		echo "$artist - $album - $name"
+		fpath="$artist/$album"
+		fname="$track - $name$ext"
+
+		# echo "Path: $fpath"
+		# echo "Name: $fname"
+
+		mkdir -p "$outputDir/$fpath"
+		mv "${args[${i}]}" "$fname" 2>/dev/null
+		mv "$fname" "$outputDir/$fpath/" 
+		echo "${args[${i}]}  >>>>  $outputDir/$fpath/$fname"
 	done
 fi
